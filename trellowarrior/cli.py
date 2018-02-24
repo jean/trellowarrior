ee0da55a2055ea4cebd7cb3a9a416dfba679163c
Jean Jordaan <jean.jordaan@gmail.com>
2018-02-24 17:33:52 +0700

    Hook up the CLI to trellowarrior proper

--- trellowarrior/cli.py
+++ trellowarrior/cli.py
@@ -3,14 +3,16 @@
 """Console script for trellowarrior."""
 
 import click
+from trellowarrior import main as trellowarrior_main 
+from trellowarrior import parse_config
 
 
 @click.command()
 def main(args=None):
     """Console script for trellowarrior."""
-    click.echo("Replace this message by putting your code into "
-               "trellowarrior.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    # TODO: make conf file location configurable
+    if parse_config('conf/trellowarrior.conf'):
+        trellowarrior_main()
 
 
 if __name__ == "__main__":
